b959a35dc7a2647d4355316f4a4ae627b09218a3
kevin <kevin70852000@gmail.com>
2021-09-08 22:01:12 +0800

    edit

--- OneDrive/桌面/vue-material-kit-master/vue.config.js
+++ OneDrive/桌面/vue-material-kit-master/vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? '/twhome/' // test20200915 為 repo 名稱
+      : '/',
   css: {
     loaderOptions: {
       css: {
