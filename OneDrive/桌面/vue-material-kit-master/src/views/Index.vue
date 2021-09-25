41db4162335289cec709c3531a2abeddb0b703f1
kevin <kevin70852000@gmail.com>
2021-09-25 14:21:25 +0800

    edit

--- OneDrive/桌面/vue-material-kit-master/src/views/Index.vue
+++ OneDrive/桌面/vue-material-kit-master/src/views/Index.vue
@@ -28,15 +28,67 @@
             <div
               class="md-layout-item md-size-66 md-xsmall-size-100 ml-auto mr-auto text-center"
             >
-              <h2>最新消息</h2>
-              <h4>可代訂烤肉食材、賞鯨套票，費用另計。</h4>
-              <h5>簡介</h5>
+              <h1 class="title">
+                <span class="tim-note">民宿簡介</span>
+              </h1>
+              <!-- <h4 class="title">
+                <span class="tim-note">民宿簡介</span>
+              </h4> -->
               <p>
                 童玩民宿位於五結交流道附近，鄰近轉運站、羅東火車站，交通十分便利，
                 距離羅東夜市、傳統藝術中心、冬山河親水公園等知名景點只需幾分鐘路程，
                 民宿鄰近還有24小時便利商店，提供最佳生活機能。
                 快來宜蘭享受專屬於你，一趟追尋幸福的假期。
               </p>
+              <h2 class="title">
+                <span class="tim-note">最新消息</span>
+              </h2>
+              <h4 class="title">
+                <span class="tim-note"
+                  >可代訂烤肉食材、賞鯨套票，費用另計。</span
+                >
+              </h4>
+              <i class="large fa fa-ship"></i>
+              <md-button
+                class="md-info md-round"
+                href="https://events.necoast-nsa.gov.tw/coast/"
+                >自行訂購賞鯨船票連結</md-button
+              >
+              <h4 class="title">
+                <span class="tim-note text-info">龜山島簡介</span>
+              </h4>
+              <p class="text-info">
+                龜山島位於宜蘭縣頭城鎮海岸以東約10公里處，是孤懸於海中之火山島嶼，因其形似浮龜而得名，更曾被國際知名自然網站《WHEN ON EARTH》列為全世界最酷的小島之一，來到東北角除了觀覽濱海美景和壯麗的巨岩礁石，現在還可搭上船航向宜蘭外海上的神祕小島，暢遊藍色公路，一路玩進龜山島！
+              </p>
+              <md-card>
+                <carousel
+                  :per-page="1"
+                  loop
+                  :speed="700"
+                  autoplay
+                  :autoplay-timeout="5000"
+                  :mouse-drag="false"
+                  navigationEnabled
+                  navigationNextLabel="<i class='material-icons'>keyboard_arrow_right</i>"
+                  navigationPrevLabel="<i class='material-icons'>keyboard_arrow_left</i>"
+                >
+                  <slide>
+                    <img :src="carousel1" alt="carousel1" />
+                  </slide>
+                  <slide>
+                    <img :src="carousel2" alt="carousel2" />
+                  </slide>
+                  <slide>
+                    <img :src="carousel3" alt="carousel3" />
+                  </slide>
+                  <!-- <slide>
+                    <img :src="carousel4" alt="carousel4" />
+                  </slide>
+                  <slide>
+                    <img :src="carousel5" alt="carousel5" />
+                  </slide> -->
+                </carousel>
+              </md-card>
               <h2>2020安心旅遊補助：</h2>
               <h4>
                 安心旅遊/健康住宿 入住觀光局百大好客民宿
@@ -118,7 +170,12 @@ export default {
       firstname: null,
       email: null,
       password: null,
-      leafShow: false
+      leafShow: false,
+      carousel1: require("@/assets/img/turtle.jpg"),
+      carousel2: require("@/assets/img/dolphlin.jpg"),
+      carousel3: require("@/assets/img/dolphlin2.jpg")
+      // carousel4: require("@/assets/img/room2-4.jpg"),
+      // carousel5: require("@/assets/img/room2-5.jpg")
     };
   },
   methods: {
